db80c63b8e36c582103415cd30591f4a8ca93e26
mankycat <bruce0115@gmail.com>
2014-08-13 18:08:22 +0800

    import

---
+++ app/src/main/java/tw/marlborocrew/com/ratetaxitw/SplashScreen.java
@@ -0,0 +1,18 @@
+package tw.marlborocrew.com.ratetaxitw;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        System.out.println("here is different what the fuck");
+    }
+
+}
